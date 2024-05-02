789cd41f7a21b0fb6006204ed8cf67791e9e4158
YOO TAEYANG <yty0706@gmail.com>
2024-05-02 21:15:16 +0900

    0502 준비 자료 업로드

---
+++ Assets/Scripts/002/CollectionTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionTest : MonoBehaviour
+{
+    // 배열, 리스트를 설명하는 게임 만들어보기
+    private int[] TestArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
